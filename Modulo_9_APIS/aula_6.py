e6ca8609c9e027a356be141cd39c9cfaafc83e77
AlyssonSmartCell <alyssonsmartcell@gmail.com>
2023-02-17 00:49:12 -0300

    teste

--- Modulo_9_APIS/aula_6.py
+++ Modulo_9_APIS/aula_6.py
@@ -8,7 +8,25 @@ resultado_get = requests.get('https://jsonplaceholder.typicode.com/todos')
 
 #Get com id
 resultado_get_com_id = requests.get('https://jsonplaceholder.typicode.com/todos/2')
-# pprint(resultado_get_com_id.json())
+#pprint(resultado_get_com_id.json())
 
 #   POST criar um novo recurso 
+nova_funcao = {'completed': False,
+ 'title': 'testar api',
+ 'userId': 1}
 
+resultado_post = requests.post('https://jsonplaceholder.typicode.com/todos', nova_funcao)
+#pprint(resultado_post.json())
+
+#Put - alterar um recurso existente 
+tarefa_alterada = {'completed': False,
+ 'title': 'testando alterada api',
+ 'userId': 1}
+
+resultado_pu = requests.put('https://jsonplaceholder.typicode.com/todos/2', tarefa_alterada)
+#pprint(resultado_pu.json())
+
+#DELETE - Para deletar uma recurso 
+
+funcao_deletada = requests.delete('https://jsonplaceholder.typicode.com/todos/2')
+pprint(funcao_deletada.json())
